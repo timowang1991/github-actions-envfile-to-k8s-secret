d0402d1c03fcfe5b50dc463fc37fdf24b8b097c2
Timo Wang <timo@steaker.com>
2022-02-09 20:30:06 +0800

    fix echo set-output

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ echo ${base64EncodedEnvString} | base64 -d > .env
 
 result=$(python main.py --name ${k8sName} --kind ${k8sKind} --env .env)
 
-echo "::set-output name=k8sOutputObject::${result}"
+echo "::set-output name=k8sObject::${result}"
