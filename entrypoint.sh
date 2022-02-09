c380a77625a22e15976f40d09d4f1ff56afe50ff
Timo Wang <timo@steaker.com>
2022-02-09 17:09:35 +0800

    debug with echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,14 @@ base64EncodedEnvString=$3
 
 base64DecodedEnvString=$(echo ${base64EncodedEnvString} | base64 -d)
 
+echo "1:$1"
+echo "2:$2"
+echo "3:$3"
+
+echo "k8sName:$k8sName"
+echo "k8sKind:$k8sKind"
+echo "base64EncodedEnvString:$base64EncodedEnvString"
+echo "base64DecodedEnvString:$base64DecodedEnvString"
+
 result=$(echo ${base64DecodedEnvString} | python main.py --name ${k8sName} --kind ${k8sKind})
 echo "::set-output name=k8sOutputObject::${result}"
