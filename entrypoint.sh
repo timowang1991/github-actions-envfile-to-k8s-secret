1f91035208203a1d08ad3130e52e41fb9a0b63b6
Timo Wang <timo@steaker.com>
2022-02-09 20:43:01 +0800

    fix multiline issue with set-output

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,8 @@ echo ${base64EncodedEnvString} | base64 -d > .env
 
 result=$(python main.py --name ${k8sName} --kind ${k8sKind} --env .env)
 
+result="${result//'%'/'%25'}"
+result="${result//$'\n'/'%0A'}"
+result="${result//$'\r'/'%0D'}"
+
 echo "::set-output name=k8sObject::${result}"
