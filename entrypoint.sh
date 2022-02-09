f8445400876a52fabefdb8d83168a50bc9c71796
Timo Wang <timo@steaker.com>
2022-02-09 17:45:08 +0800

    fix syntax error typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ base64EncodedEnvString=$3
 
 echo ${base64EncodedEnvString} | base64 -d > .env
 
-result=$(python main.py --name ${k8sName} --kind ${k8sKind}) --env .env)
+result=$(python main.py --name ${k8sName} --kind ${k8sKind} --env .env)
 
 echo "::set-output name=k8sOutputObject::${result}"
